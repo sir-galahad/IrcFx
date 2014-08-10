2677c2ce4b626b2b14b982e1be5c8ad2be1ef63d
Aaron Davis <aarond_01@yahoo.com>
2014-08-10 16:17:55 -0400

    bleh

---
+++ IrcFx/AnotherTest.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aaron
+ * Date: 8/10/2014
+ * Time: 4:17 PM
+ * 
+ * 
+ */
+using System;
+
+namespace IrcFx
+{
+	/// <summary>
+	/// Description of AnotherTest.
+	/// </summary>
+	public class AnotherTest
+	{
+		public AnotherTest()
+		{
+		}
+	}
+}
